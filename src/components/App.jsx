84118228a06df484bb72a69ba9fc8ea51721ede2
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 08:44:20 +0300

    connect stacks to main component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import LinkedLists from './LinkedLists'
 import '../styles/app.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Stacks from './Stack'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/linked_lists" element={<LinkedLists />} />
+        <Route path="/stacks" element={<Stacks />} />
       </Routes>
     </BrowserRouter>
   )
