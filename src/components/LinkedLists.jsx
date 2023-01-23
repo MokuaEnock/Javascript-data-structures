ca54cba5257c5f6fd23aa122ef19b3ae91935a78
Enock <enock.mokua@student.moringaschool.com>
2023-01-23 07:15:35 +0300

    add notes for linked lists

--- src/components/LinkedLists.jsx
+++ src/components/LinkedLists.jsx
@@ -1,3 +1,28 @@
 export default function LinkedLists() {
-  return <main></main>
+  return (
+    <main>
+      <h2>Linked Lists</h2>
+      <p>There are two types of linked lists</p>
+      <p>
+        Linear collection of data elements linear order is not given by their
+        physical placement in memory. Each elemnt points to the next. Consist of
+        a group of nodes which togetehr represent a sequence. Each node consits
+        of data and a reference. The access time is linear since you have to
+        traverse through each element in the list and difficult to pipeline.
+        Have better cache locality.
+      </p>
+      <ol>
+        <li>Singly Linked Lists</li>
+        <li>Doubly linked lists</li>
+      </ol>
+      <p>
+        Each element in a linked lists has a refernce that points to the next
+        element in the list.
+      </p>
+      <p>
+        Each node in a doubly linked list has a refernce to the nex and previous
+        element.
+      </p>
+    </main>
+  )
 }
